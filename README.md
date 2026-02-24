5699fd2791b3e3fc5871e061183fc59ffe5d6362
Doriandj9 <dorian9armijos@gmail.com>
2026-02-24 08:53:35 -0500

    Inicializacion de app lugar de votacion

---
+++ README.md
@@ -0,0 +1,2 @@
+# cn_consulta_lugar_votacion
+
---
+++ android/app/src/main/kotlin/com/example/cn_consulta_lugar_votacion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cn_consulta_lugar_votacion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
