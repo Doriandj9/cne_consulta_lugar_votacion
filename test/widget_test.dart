416266d1ca3c5d65a462bb3485005f97a7c197dd
Doriandj9 <dorian9armijos@gmail.com>
2026-02-24 08:59:14 -0500

    Cambio de cn a cne

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cn_consulta_lugar_votacion/main.dart';
+import 'package:cne_consulta_lugar_votacion/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
